3013f423dfdf7fabb1f0ef94d535d597dab2b478
Ada Cheng <adafycheng2020@gmail.com>
2022-02-24 20:39:14 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,30 @@
+# reactjs-topnav-component
+
+> A Top Navigation bar component using react.js
+
+[![NPM](https://img.shields.io/npm/v/reactjs-topnav-component.svg)](https://www.npmjs.com/package/reactjs-topnav-component) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+## Install
+
+```bash
+npm install --save reactjs-topnav-component
+```
+
+## Usage
+
+```jsx
+import React, { Component } from 'react'
+
+import MyComponent from 'reactjs-topnav-component'
+import 'reactjs-topnav-component/dist/index.css'
+
+class Example extends Component {
+  render() {
+    return <MyComponent />
+  }
+}
+```
+
+## License
+
+MIT © [adafycheng](https://github.com/adafycheng)
---
+++ example/src/App.js
@@ -0,0 +1,66 @@
+import React from 'react'
+
+import { TopNav, TopNavItem } from 'reactjs-topnav-component'
+import 'reactjs-topnav-component/dist/index.css'
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+
+const linksData = {
+  items: [
+    {
+      alt:  'GitHub',
+      src:  './images/github.png',
+      href: 'https://github.com/adafycheng'
+    },
+    {
+      alt:  'LinkedIn',
+      src:  './images/linkedin.png',
+      href: 'https://linkedin.com/in/adafycheng'
+    },
+    {
+      alt:  'stackoverflow',
+      src:  './images/stackoverflow.png',
+      href: 'https://stackoverflow.com/story/adafycheng'
+    },
+    {
+      alt:  'Twitter',
+      src:  './images/twitter.png',
+      href: 'https://twitter.com/adafycheng'
+    },
+    {
+      alt:  'Google Developer',
+      src:  './images/gdev.png',
+      href: 'https://g.dev/adafycheng'
+    },
+    {
+      alt:  'CodePen',
+      src:  './images/codepen.png',
+      href: 'https://codepen.io/adafycheng'
+    },
+    {
+      alt:  'HashNode',
+      src:  './images/hashnode.png',
+      href: 'https://adafycheng.hashnode.dev'
+    },
+    {
+      alt:  'dev.to',
+      src:  './images/dev-black.png',
+      href: 'https://dev.to/adafycheng'
+    }
+  ]
+}
+
+const App = () => {
+  return (
+    <TopNav navTitle="My Portfolio" links={linksData}>
+      <TopNavItem href="#main">Home</TopNavItem>
+      <TopNavItem href="#about">About</TopNavItem>
+      <TopNavItem href="#portfolio">Portfolio</TopNavItem>
+      <TopNavItem href="#contact">Contact</TopNavItem>
+    </TopNav>
+  );
+}
+
+export default App
