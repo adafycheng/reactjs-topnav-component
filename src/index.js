f8bc7238752c4f08a2213755f8706323a5e2e04b
Ada Cheng <adafycheng2020@gmail.com>
2022-03-04 13:57:24 +0000

    Fix problems found when running `npm run test:lint.

--- src/index.js
+++ src/index.js
@@ -1,47 +1,67 @@
-import React from 'react';
-import {Nav} from 'react-bootstrap';
-import $ from 'jquery';
+import React from 'react'
+import { Nav } from 'react-bootstrap'
+import $ from 'jquery'
 import styles from './styles.module.css'
 
-
 export const TopNav = (props) => {
-    $(function () {
-        // read the input JSON content
-        if (props.links !== undefined) {
-            console.log('props.links.items.length: ' + props.links.items.length)
-            $('#navbarLinksDev').empty()
-            for (let i = 0; i < props.links.items.length; i++) {
-                const newDiv = $('<a href="' + props.links.items[i].href + '" target="_blank" rel="noopener noreferrer"><img src=' + props.links.items[i].src + ' class=' + styles.icon + ' alt="' + props.links.items[i].alt + '" /></a>')
-                $('#navbarLinksDev').append(newDiv).append("&nbsp;")
-            }
-        }
-    })
+  $(function () {
+    // read the input JSON content
+    if (props.links !== undefined) {
+      console.log('props.links.items.length: ' + props.links.items.length)
+      $('#navbarLinksDev').empty()
+      for (let i = 0; i < props.links.items.length; i++) {
+        const newDiv = $(
+          '<a href="' +
+            props.links.items[i].href +
+            '" target="_blank" rel="noopener noreferrer"><img src=' +
+            props.links.items[i].src +
+            ' class=' +
+            styles.icon +
+            ' alt="' +
+            props.links.items[i].alt +
+            '" /></a>'
+        )
 
-    const navbarClassName = "navbar navbar-expand-lg navbar-light bg-light " + styles.header;
+        $('#navbarLinksDev').append(newDiv).append('&nbsp;')
+      }
+    }
+  })
 
-    return (
-        <Nav className={navbarClassName}>
-            <div className="container-fluid">
-                <a className={styles.navbarBrand} href="#main">{ props.navTitle }</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                      { props.children }
-                    </ul>
-                    <div id="navbarLinksDev"></div>
-                </div>
-            </div>
-        </Nav>
-    );
+  const navbarClassName =
+    'navbar navbar-expand-lg navbar-light bg-light ' + styles.header
+
+  return (
+    <Nav className={navbarClassName}>
+      <div className='container-fluid'>
+        <a className={styles.navbarBrand} href='#main'>
+          {props.navTitle}
+        </a>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#navbarSupportedContent'
+          aria-controls='navbarSupportedContent'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span className='navbar-toggler-icon' />
+        </button>
+        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+          <ul className='navbar-nav me-auto mb-2 mb-lg-0'>{props.children}</ul>
+          <div id='navbarLinksDev' />
+        </div>
+      </div>
+    </Nav>
+  )
 }
 
-
 export const TopNavItem = (props) => {
-    return (
-        <li className={`nav-item`}>
-          <a className={styles.navLink}  href={props.href}>{props.children}</a>
-        </li>
-    );
+  return (
+    <li className='nav-item'>
+      <a className={styles.navLink} href={props.href}>
+        {props.children}
+      </a>
+    </li>
+  )
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,46 +1,46 @@
-import { TopNav, TopNavItem } from '.'
+import { TopNav } from '.'
 import React from 'react'
 
 const linksData = {
   items: [
     {
-      alt:  'GitHub',
-      src:  './images/github.png',
+      alt: 'GitHub',
+      src: './images/github.png',
       href: 'https://github.com/adafycheng'
     },
     {
-      alt:  'LinkedIn',
-      src:  './images/linkedin.png',
+      alt: 'LinkedIn',
+      src: './images/linkedin.png',
       href: 'https://linkedin.com/in/adafycheng'
     },
     {
-      alt:  'stackoverflow',
-      src:  './images/stackoverflow.png',
+      alt: 'stackoverflow',
+      src: './images/stackoverflow.png',
       href: 'https://stackoverflow.com/story/adafycheng'
     },
     {
-      alt:  'Twitter',
-      src:  './images/twitter.png',
+      alt: 'Twitter',
+      src: './images/twitter.png',
       href: 'https://twitter.com/adafycheng'
     },
     {
-      alt:  'Google Developer',
-      src:  './images/gdev.png',
+      alt: 'Google Developer',
+      src: './images/gdev.png',
       href: 'https://g.dev/adafycheng'
     },
     {
-      alt:  'CodePen',
-      src:  './images/codepen.png',
+      alt: 'CodePen',
+      src: './images/codepen.png',
       href: 'https://codepen.io/adafycheng'
     },
     {
-      alt:  'HashNode',
-      src:  './images/hashnode.png',
+      alt: 'HashNode',
+      src: './images/hashnode.png',
       href: 'https://adafycheng.hashnode.dev'
     },
     {
-      alt:  'dev.to',
-      src:  './images/dev-black.png',
+      alt: 'dev.to',
+      src: './images/dev-black.png',
       href: 'https://dev.to/adafycheng'
     }
   ]
@@ -48,7 +48,7 @@ const linksData = {
 
 describe('TopNav', () => {
   it('is truthy', () => {
-    const nav = <TopNav navTitle="My Portfolio" links={linksData} />
+    const nav = <TopNav navTitle='My Portfolio' links={linksData} />
     expect(nav).toBeTruthy()
   })
 })
